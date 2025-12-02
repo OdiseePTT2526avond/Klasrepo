75556639bca54d92f0faa3ea8c86f1c6b9d3911a
Maarten Troost <maarten.troost@odisee.be>
2025-12-02 20:51:24 +0100

    Oefeningen week 2

---
+++ Oefening_week2_Garagepoort/opgave/Start_Project_Garagepoort/Garage/Garage.cs
@@ -0,0 +1,33 @@
+﻿namespace Garage;
+public class Garage
+{
+    public int capacity; // 200
+    public int occupancy; // 0 -> 10 -> 200
+
+    public Garage(int capacity) {
+        if (capacity < 0)
+        {
+            throw new ArgumentException("Een Garage kan geen negatieve capaciteit hebben");
+        }
+
+        this.capacity = capacity;
+        this.occupancy = 0;
+    }
+
+    public bool IsFull()
+    {
+        return occupancy == capacity;
+    }
+
+    public bool IsEmpty()
+    {
+        return occupancy == 0;
+    }
+
+    public void Enter(Vehicle v) {
+        occupancy += 1;
+    }
+}
+
+
+
